e19bc0faa5c90193478a89a7efd7d826b4b00c41
unknown <chukwagon1@gmail.com>
2024-07-25 05:05:35 -0700

    Fix: nothing happened just tried somehting

--- src/app/providers/ReduxProvider.tsx
+++ src/app/providers/ReduxProvider.tsx
@@ -1,23 +1,28 @@
-"use client";
-import React from "react";
+'use client'
+import React from 'react'
 import { Provider } from "react-redux";
-import { AppStore, makeStore } from "../redux/store";
+import { AppStore, makeStore } from '../redux/store';
 import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from 'redux-persist/integration/react';
 
-const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
-  const storeRef = React.useRef<AppStore>();
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-  }
-  const persistor = persistStore(storeRef.current);
+const ReduxProvider = ({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) => {
+
+    const storeRef = React.useRef<AppStore>()
+    if (!storeRef.current) {
+      storeRef.current = makeStore()
+    }
+    const persistor = persistStore(storeRef.current);
   return (
     <Provider store={storeRef.current}>
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
+       <PersistGate loading={null} persistor={persistor}>
+       {children}
+       </PersistGate>
     </Provider>
-  );
-};
+  )
+}
 
-export default ReduxProvider;
+export default ReduxProvider
