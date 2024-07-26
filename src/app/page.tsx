d45a7b60b7ac93e0f81b78d9c18e7ba69ebce344
unknown <chukwagon1@gmail.com>
2024-07-25 20:15:55 -0700

    Refacor: Going to work on left drawer next ... going to use framer motion and css and zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ThemeSwitch />
+      <p>Next Line of action is working on left side drawer witht the three lines icon</p>
     </main>
   );
 }
